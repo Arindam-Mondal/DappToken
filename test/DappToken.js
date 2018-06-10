35904a5dfe2e43e1df3c8bf1f7c98b91983ea46e
arindam <arindamm@mindfiresolutions.com>
2018-06-10 08:54:37 +0530

    Token Transfer functionality implemented

--- test/DappToken.js
+++ test/DappToken.js
@@ -57,4 +57,68 @@ contract('DappToken',function(accounts){
         });
     });
 
+    
+    it('approve tokens for delegated transfer', function(){
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.approve(accounts[1],100, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers 1 event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized By');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transferred amount');
+            return tokenInstance.allowance(accounts[0],accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+        });
+    });
+
+    it('handles delegated token transfer',function(){
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            
+            // Transfer some tokens to fromAccount
+            return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+        }).then(function(receipt){
+            // Approve spending account to send 10 tokens from fromAccount
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(function(receipt){
+            // Transferring something larger than sender's balance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount}); 
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value greater than balance');
+            //try sending amount greater than approved amount
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function(success){
+            assert.equal(success,true,'amount transferred');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers 1 event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs the transferred amount');
+
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 10, 'add the amount to the receiving account');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), 0, 'balance should be 0')
+        });
+    });
+
 });
